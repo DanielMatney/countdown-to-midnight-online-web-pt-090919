995bd10a17191bde3c799ec2af8f972c2d661dac
Daniel Matney <danieljmatney@gmail.com>
2019-09-16 06:58:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,17 +2,17 @@
 
 def countdown(seconds)
   while seconds > 0
-    puts "#{seconds} SECOND(S)!"
+    p "#{seconds} SECOND(S)!"
     seconds -= 1
   end
-  puts "HAPPY NEW YEAR!"
+  p "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(seconds)
   while seconds > 0
-    puts "#{seconds} SECOND(S)!"
+    p "#{seconds} SECOND(S)!"
     seconds -= 1
     sleep(1)
   end
-  puts "HAPPY NEW YEAR!"
+  p "HAPPY NEW YEAR!"
 end
