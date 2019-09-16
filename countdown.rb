5b0fa4b543037f0eac1ad50c0e390aacc52449f3
Daniel Matney <danieljmatney@gmail.com>
2019-09-16 06:57:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ def countdown(seconds)
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
   end
-  p "HAPPY NEW YEAR!"
+  puts "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(seconds)
@@ -14,5 +14,5 @@ def countdown_with_sleep(seconds)
     seconds -= 1
     sleep(1)
   end
-  p "HAPPY NEW YEAR!"
+  puts "HAPPY NEW YEAR!"
 end
